2b7cd33cf76e553c73be061b81a1c2dda1777c58
Alexey <fedorov-alexey-vladimirovich@yandex.com>
2020-05-03 00:32:43 +0300

    Move middleware declaration before routes

--- server/src/app.js
+++ server/src/app.js
@@ -6,11 +6,11 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.resolve('../client/build')));
+
 app.get('/', (req, res) => res.sendFile(path.resolve('../client/build/index.html')));
 app.get('/poems', (req, res) => res.sendFile(path.resolve('./data/poems.json')));
-
-app.use(express.json());
-app.use(express.static(path.resolve('../client/build')));
-app.use(cors());
 
 module.exports = app;
