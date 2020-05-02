e3ef0ab27d34c24edf816efec201f1a00d6559ab
Alexey <fedorov-alexey-vladimirovich@yandex.com>
2020-05-02 16:17:02 +0300

    Add endpoints

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,15 @@
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Привет!'));
+app.get('/', (req, res) => res.sendFile(path.resolve('../client/build/index.html')));
+app.get('/poems', (req, res) => res.sendFile(path.resolve('./data/poems.json')));
 
 app.use(express.json());
+app.use(express.static(path.resolve('../client/build')));
 app.use(cors());
 
 module.exports = app;
