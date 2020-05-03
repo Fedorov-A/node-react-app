034be79b6c00f4c1c2b5f62dfdbb15d8e9b0980a
Alexey <fedorov-alexey-vladimirovich@yandex.com>
2020-05-03 19:39:10 +0300

    Add routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,67 @@
 import React from 'react';
 import Axios from 'axios';
-import { Button, Table } from 'antd';
+import { Button, Table, Card } from 'antd';
+import { Link, Route, Switch } from 'react-router-dom';
 import './App.css';
+import '.'
 
 class App extends React.Component {
-    constructor(props) 
+  render() {
+    return <div className="App-header">
+      <Hello />
+      <Main />
+    </div>;
+  }
+}
+
+class Hello extends React.Component {
+  render() {
+    return <div className="Margin">Привет!</div>;
+  }
+}
+
+class Main extends React.Component {
+  render() {
+    return <Switch>
+      <Route exact path="/" component={Poems}/>
+      <Route path="/author" component={AuthorInfo} />}/>
+    </Switch>
+  }
+}
+
+class Poems extends React.Component {
+  constructor(props) 
     {
       super(props);
-      this.state = { data: [] };
-      this.state = { columns: [] };
+      this.state = { 
+        data: [],
+        columns: []
+      };
     }
 
     render() {
-      return <div className="App">
-        <header className="App-header">
-          <p>
-            Привет!
-          </p>
-          <Table dataSource={this.state.data} columns={this.state.columns}></Table>
-        </header>
-      </div>;
+      return <Table dataSource={this.state.data} columns={this.state.columns}></Table>;
     }
 
     componentDidMount() {
       Axios.get('http://localhost:8800/poems')
       .then((res) => {
-        this.setState(() => ({ 
+        this.setState(() => ({
           data: res.data,
           columns: [
             {
               title: 'Наименование книги',
               dataIndex: 'name',
-              key: 'name',
             },
             {
               title: 'Автор',
-              dataIndex: 'author.name',
-              key: 'author.name',
+              render: (data) => (<li>
+                <Link to={{ pathname: "/author", state: data.author }}> {data.author.name} </Link>
+              </li>)
             },
             {    
               title: 'Дата',
               dataIndex: 'date',
-              key: 'date',
             }
           ]}));
       })
@@ -49,6 +69,19 @@ class App extends React.Component {
         console.log(err);
       });
     }
+}
+
+class AuthorInfo extends React.Component {
+  render() {
+    return <>
+      <Card style={{ width: 240 }} cover={<img src={this.props.location.state.photo}></img>}>
+        <Card.Meta title={`${this.props.location.state.birthDate} - ${this.props.location.state.deathDate}`} />
+      </Card>
+      <Button className="Margin">
+        <Link to="/">Назад</Link>
+      </Button>
+      </>;
+  }
 }
 
 export default App;
